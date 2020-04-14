f6ae0ec4d0c385ce0258d48d929bf93a07eb03e3
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-04-14 15:56:47 -0400

    Renamed file

---
+++ src/components/ClientWork/data/data.js
@@ -0,0 +1,13 @@
+const projectsLarge = [
+    {
+        title: "Drip on the Wall (Work in Progress)",
+        desc: "Drip on the Wall is a residential and commercial painting company based in California",
+        img: "./images/DotW-min.jpg",
+        imgAlt: "Drip on the Wall Thumbnail",
+        live: "https://heuristic-johnson-ea569b.netlify.com/",
+        github: "https://github.com/ThomasWicksWeb/DripOnTheWall",
+        techUsed: "Bulma"
+    }
+]
+
+export default projectsLarge;
