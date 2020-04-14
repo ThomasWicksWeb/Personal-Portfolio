6c1935af1ae58bafcc85144592d3c5469422b9d6
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-04-14 15:58:07 -0400

    Renamed file

---
+++ src/components/PersonalProjects/data/index.js
@@ -0,0 +1,31 @@
+const personalProjectsLarge = [{
+    title: "Web Dev Resources",
+    desc: "A collection of resources I've accumulated, put into nicely organized lists for anybody to use. Currently contains over 50 resources -- that's at least 51 links, woah!",
+    img: "./images/WebDevResources-min.jpg",
+    imgAlt: "Web Dev Resources Thumbnail",
+    live: "https://www.WebDevResources.info",
+    github: "https://github.com/ThomasWicksWeb/Web-Dev-Resources",
+    techUsed: "React, Bulma"
+}]
+
+const personalProjectsSmall = [{
+        title: "German Word-A-Day!",
+        desc: "Learn a new German word everyday! Refreshes locally everyday at midnight.",
+        img: "./images/germanwordaday-min.jpg",
+        imgAlt: "German Word-A-Day! Thumbnail",
+        live: "https://germanwordaday.info",
+        github: "https://github.com/ThomasWicksWeb/German-Word-A-Day",
+        techUsed: "React"
+    },
+    {
+        title: "Tesla Tracker",
+        desc: "A website used for tracking the sales of Tesla models from 2015 to 2018.",
+        img: "./images/teslaBackground.jpg",
+        imgAlt: "Tesla Tracker Thumbnail",
+        live: "./Projects/TeslaStatsWIP/index.html",
+        github: "https://github.com/ThomasWicksWeb/Tesla-Tracker",
+        techUsed: "Bulma, Charts JS"
+    }
+]
+
+export { personalProjectsLarge, personalProjectsSmall };
