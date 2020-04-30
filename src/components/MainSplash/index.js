3ab14e7c2c22d1551711d7574849d9b5ddf8d860
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-04-30 18:52:33 -0400

    Update index.js

--- src/components/MainSplash/index.js
+++ src/components/MainSplash/index.js
@@ -28,7 +28,7 @@ const MainSplash = ({currentHourOfDay}) => {
                 <p className="is-size-5">Down below are some of my projects</p>
                 <p className="is-size-5 zFix">Alternatively, you can view my 
                     <InternalLink 
-                        href="https://thomaswicks.com/MyResume/ThomasWicksResume.pdf"
+                        href="https://thomaswicks.com/resume/"
                         text="résumé"
                         emoji="📝"
                     />
