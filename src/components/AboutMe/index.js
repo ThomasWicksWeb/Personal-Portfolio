2773cf6bced318c27e5f6aa8b187613dd02a1421
ThomasWicksWeb <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-05-17 10:54:11 -0400

    Updated 'About Me'

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -17,7 +17,7 @@ const AboutMe = () => {
           <div className="projectsSmallGrid AboutMeColumns">
             <div>
               <p className="is-size-5">
-                I'll be graduating from Farmingdale State College in May of 2020
+                I've recently graduated from Farmingdale State College this last May
                 with a Bachelor's degree in Computer Programming &amp;
                 Information Systems
                 <Emoji label="School building" emoji="🏛️" />
