e57c5ce470f1b9f3443eea559a64cc03c4f80887
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-06-27 11:40:35 -0400

    Refactor: ContactMe component now exports from a default index.jsx file

---
+++ src/components/ContactMe/ContactMe.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import SectionHeader from '../Misc/SectionHeader'
+import './ContactMe.scss';
+
+const ContactMe = () => {
+    return(
+        <section id="contactMe" className="section contactMe">
+            <div className="container">
+            <SectionHeader emoji="✉️" label="Folder emoji" HeaderText="Contact Me" />
+                <form className="form" action="https://formspree.io/thomaswicks.ch@gmail.com" method="POST">
+                    <div className="field">
+                        <label htmlFor="name" className="label is-size-5">Name</label>
+                        <div className="control">
+                            <input id="name" className="input" type="text" placeholder="Your name" name="name" />
+                        </div>
+                    </div>
+
+                    <div className="field">
+                        <label htmlFor="email" className="label is-size-5">Email</label>
+                        <div className="control">
+                            <input id="email" name="email" className="input" type="email" placeholder="Your email" />
+                        </div>
+                    </div>
+
+                    <div className="field">
+                        <label htmlFor="message" className="label is-size-5">Message</label>
+                        <div className="control">
+                            <textarea id="message" className="textarea" placeholder="Hey there" name="message"></textarea>
+                        </div>
+                    </div>
+
+                    <div className="field is-grouped">
+                        <div className="control">
+                            <button value="submit" type="submit" className="button is-link">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+    )
+}
+
+export default ContactMe;
