7817682c9bfa196e874052d8246d7359e187c699
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-06-27 11:39:41 -0400

    Update: Now dereferences components due to change in file structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import TopBar from './components/TopBar';
-import MainSplash from './components/MainSplash';
-import AboutMe from './components/AboutMe';
-import ClientWork from './components/ClientWork';
-import PersonalProjects from './components/PersonalProjects';
-import ContactMe from './components/ContactMe'
-import AppFooter from './components/Footer'
+import React from "react";
+import { TopBar } from "./components/TopBar";
+import { MainSplash } from "./components/MainSplash";
+import { AboutMe } from "./components/AboutMe";
+import { ClientWork } from "./components/ClientWork";
+import { PersonalProjects } from "./components/PersonalProjects";
+import { ContactMe } from "./components/ContactMe";
+import { Footer } from "./components/Footer";
 
-import './style.scss';
+import "./style.scss";
 
 function App() {
-
-// Used for the dark mode slider in <TopBar /> and to set the greeting in <MainSplash />
-const currentHourOfDay = new Date().getHours();
+  // Used for the dark mode slider in <TopBar /> and to set the greeting in <MainSplash />
+  const currentHourOfDay = new Date().getHours();
 
   return (
     <main>
-      <TopBar currentHourOfDay={ currentHourOfDay } />
-      <MainSplash currentHourOfDay={ currentHourOfDay } />
+      <TopBar currentHourOfDay={currentHourOfDay} />
+      <MainSplash currentHourOfDay={currentHourOfDay} />
       <AboutMe />
       <ClientWork />
       <PersonalProjects />
       <ContactMe />
-      <AppFooter />
+      <Footer />
     </main>
   );
 }
