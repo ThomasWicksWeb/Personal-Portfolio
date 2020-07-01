9b9f9f49abaac721661558469d730125e1e0d3d1
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-07-01 10:37:47 -0400

    Update: SectionHeader is now a styled component

--- src/components/Misc/SectionHeader/index.js
+++ src/components/Misc/SectionHeader/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import Emoji from '../Emoji';
-import classnames from 'classnames';
-import styles from './SectionHeader.module.scss';
+import React, { useContext } from "react";
+import Emoji from "../Emoji";
+import classnames from "classnames";
+import styles from "./SectionHeader.module.scss";
 
-const SectionHeader = ({HeaderText, emoji, label, classes}) => {
-    return(
-        <div className={classnames("container", styles.bannerTextContainer)}>
-            <h3 className={ classnames("is-size-2", styles.sectionHeader) }>{ HeaderText } <Emoji emoji={ emoji } label={ label } classes={ classes } /></h3>
-        </div>
-    )
-}
+import { ThemeContext } from "../../../contexts/ThemeContext";
+
+const SectionHeader = ({ HeaderText, emoji, label, classes }) => {
+  const { IsLightTheme, Theme } = useContext(ThemeContext);
+  const LocalTheme = IsLightTheme ? Theme.light : Theme.dark;
+
+  return (
+    <div className={classnames("container", styles.bannerTextContainer)}>
+      <h3
+        className={classnames("is-size-2", styles.sectionHeader)}
+        style={{ color: LocalTheme.syntax }}
+      >
+        {HeaderText} <Emoji emoji={emoji} label={label} classes={classes} />
+      </h3>
+    </div>
+  );
+};
 
 export default SectionHeader;
