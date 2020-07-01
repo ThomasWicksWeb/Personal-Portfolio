f1d557aba53717a44ef661298b3390890963c337
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-07-01 10:36:52 -0400

    Update: ContactMe is now a styled component

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -1,43 +1,99 @@
-import React from 'react';
-import SectionHeader from '../Misc/SectionHeader'
-import './ContactMe.scss';
+import React, { useContext } from "react";
+import SectionHeader from "../Misc/SectionHeader";
+import "./ContactMe.scss";
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 const ContactMe = () => {
-    return(
-        <section id="contactMe" className="section contactMe">
-            <div className="container">
-            <SectionHeader emoji="✉️" label="Folder emoji" HeaderText="Contact Me" />
-                <form className="form" action="https://formspree.io/thomaswicks.ch@gmail.com" method="POST">
-                    <div className="field">
-                        <label htmlFor="name" className="label is-size-5">Name</label>
-                        <div className="control">
-                            <input id="name" className="input" type="text" placeholder="Your name" name="name" />
-                        </div>
-                    </div>
+  const { IsLightTheme, Theme } = useContext(ThemeContext);
+  const LocalTheme = IsLightTheme ? Theme.light : Theme.dark;
 
-                    <div className="field">
-                        <label htmlFor="email" className="label is-size-5">Email</label>
-                        <div className="control">
-                            <input id="email" name="email" className="input" type="email" placeholder="Your email" />
-                        </div>
-                    </div>
-
-                    <div className="field">
-                        <label htmlFor="message" className="label is-size-5">Message</label>
-                        <div className="control">
-                            <textarea id="message" className="textarea" placeholder="Hey there" name="message"></textarea>
-                        </div>
-                    </div>
-
-                    <div className="field is-grouped">
-                        <div className="control">
-                            <button value="submit" type="submit" className="button is-link">Submit</button>
-                        </div>
-                    </div>
-                </form>
+  return (
+    <section
+      id="contactMe"
+      className="section contactMe"
+      style={{
+        backgroundColor: LocalTheme.backgroundColorDark,
+        color: LocalTheme.syntax,
+      }}
+    >
+      <div className="container">
+        <SectionHeader
+          emoji="✉️"
+          label="Folder emoji"
+          HeaderText="Contact Me"
+        />
+        <form
+          className="form"
+          action="https://formspree.io/thomaswicks.ch@gmail.com"
+          method="POST"
+        >
+          <div className="field">
+            <label
+              htmlFor="name"
+              className="label is-size-5"
+              style={{ color: LocalTheme.syntax }}
+            >
+              Name
+            </label>
+            <div className="control">
+              <input
+                id="name"
+                className="input"
+                type="text"
+                placeholder="Your name"
+                name="name"
+              />
             </div>
-        </section>
-    )
-}
+          </div>
+
+          <div className="field">
+            <label
+              htmlFor="email"
+              className="label is-size-5"
+              style={{ color: LocalTheme.syntax }}
+            >
+              Email
+            </label>
+            <div className="control">
+              <input
+                id="email"
+                name="email"
+                className="input"
+                type="email"
+                placeholder="Your email"
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <label
+              htmlFor="message"
+              className="label is-size-5"
+              style={{ color: LocalTheme.syntax }}
+            >
+              Message
+            </label>
+            <div className="control">
+              <textarea
+                id="message"
+                className="textarea"
+                placeholder="Hey there"
+                name="message"
+              ></textarea>
+            </div>
+          </div>
+
+          <div className="field is-grouped">
+            <div className="control">
+              <button value="submit" type="submit" className="button is-link">
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default ContactMe;
