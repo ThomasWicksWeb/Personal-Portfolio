8803b5a8d7cf0569337ea36c37b79173a73a0b5c
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-07-01 10:36:59 -0400

    Update: Footer is now a styled component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,51 @@
-import React from 'react';
-import ExternalLink from '../Misc/ExternalLink';
-import './AppFooter.scss';
+import React, { useContext } from "react";
+import ExternalLink from "../Misc/ExternalLink";
+import "./AppFooter.scss";
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 const ContactMe = () => {
-    return(
-        <footer className="footer block">
-            <div className="content has-text-centered block">
-                <p className="is-size-5">Built with <i className="fas fa-heart redHeart"></i> by <strong>Thomas Wicks</strong> in [rainy,
-                    sunny, hot, humid, cold] New York</p>
-                <p className="is-size-5 minH footerIcons redHeart negMarginLeft">
-                    <ExternalLink text="GitHub " icon="fab fa-github is-size-5 icons noWrap" label="GitHub" href="https://github.com/ThomasWicksWeb" />
+  const { IsLightTheme, Theme } = useContext(ThemeContext);
+  const LocalTheme = IsLightTheme ? Theme.light : Theme.dark;
 
-                    <ExternalLink text="LinkedIn " icon="fas fa-external-link-alt is-size-5 icons noWrap" label="LinkedIn" href="https://www.linkedin.com/in/thomas-wicks-280b5717a/" />
+  return (
+    <footer
+      className="footer block"
+      style={{
+        backgroundColor: LocalTheme.backgroundColorLight,
+        color: LocalTheme.syntax,
+      }}
+    >
+      <div className="content has-text-centered block">
+        <p className="is-size-5">
+          Built with <i className="fas fa-heart redHeart"></i> by{" "}
+          <strong style={{ color: LocalTheme.syntax }}>Thomas Wicks</strong> in
+          [rainy, sunny, hot, humid, cold] New York
+        </p>
+        <p className="is-size-5 minH footerIcons redHeart negMarginLeft">
+          <ExternalLink
+            text="GitHub "
+            icon="fab fa-github is-size-5 icons noWrap"
+            label="GitHub"
+            href="https://github.com/ThomasWicksWeb"
+          />
 
-                    <ExternalLink text="Résumé " icon="fas fa-file-alt icons noWrap" label="Résumé" href="https://www.thomaswicks.com/resume.pdf" />
-                </p>
-            </div>
-        </footer>
-    )
-}
+          <ExternalLink
+            text="LinkedIn "
+            icon="fas fa-external-link-alt is-size-5 icons noWrap"
+            label="LinkedIn"
+            href="https://www.linkedin.com/in/thomas-wicks-280b5717a/"
+          />
+
+          <ExternalLink
+            text="Résumé "
+            icon="fas fa-file-alt icons noWrap"
+            label="Résumé"
+            href="https://www.thomaswicks.com/resume.pdf"
+          />
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 export default ContactMe;
