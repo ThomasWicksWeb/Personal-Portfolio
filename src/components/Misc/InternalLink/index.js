dcc331b53b1cc69461d510ad8b3bbfa8345131be
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-07-01 10:37:32 -0400

    Update: InternalLink is now a styled component

--- src/components/Misc/InternalLink/index.js
+++ src/components/Misc/InternalLink/index.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import classnames from "classnames";
 import styles from "./InternalLink.module.scss";
 
+import { ThemeContext } from "../../../contexts/ThemeContext";
+
 const InternalLink = ({ text, href, emoji }) => {
+  const { IsLightTheme, Theme } = useContext(ThemeContext);
+  const LocalTheme = IsLightTheme ? Theme.light : Theme.dark;
+
   return (
     <a
       target="_blank"
       rel="noopener noreferrer"
       href={href}
       className={classnames("noWrap zFix", styles.zFix)}
+      // style={{ backgroundColor: LocalTheme.textBackground }}
     >
       <span className={classnames("borderMarker", styles.borderMarker)}>
-        <strong>{text}</strong> {emoji}
+        <strong style={{ color: LocalTheme.syntax }}>{text}</strong> {emoji}
       </span>
     </a>
   );
