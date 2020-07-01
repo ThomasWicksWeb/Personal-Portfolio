fef28245c1b773799a4b9070327101455d7852ba
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-07-01 10:39:07 -0400

    Update: WicksLogo is now a styled component

--- src/components/TopBar/WicksLogo/index.js
+++ src/components/TopBar/WicksLogo/index.js
@@ -1,14 +1,23 @@
-import React from 'react'
-import styles from './WicksLogo.module.scss';
-import classnames from 'classnames';
+import React, { useContext } from "react";
+import styles from "./WicksLogo.module.scss";
+import classnames from "classnames";
+
+import { ThemeContext } from "../../../contexts/ThemeContext";
 
 const WicksLogo = () => {
-    return(
-        <div id="LogoID" className={classnames("logoLight", styles.logoLight, styles.logo) }>
-            <span>WI</span>
-            <span className={ styles.negMargin }>CKS.</span>
-        </div>
-    );
-}
+  const { IsLightTheme, Theme } = useContext(ThemeContext);
+  const LocalTheme = IsLightTheme ? Theme.light : Theme.dark;
+
+  return (
+    <div
+      id="LogoID"
+      className={classnames("logoLight", styles.logoLight, styles.logo)}
+      style={{ border: LocalTheme.wicksLogoBorder, color: LocalTheme.syntax }}
+    >
+      <span>WI</span>
+      <span className={styles.negMargin}>CKS.</span>
+    </div>
+  );
+};
 
 export default WicksLogo;
