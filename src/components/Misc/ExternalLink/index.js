e2f8937f532edd65820f8badc3c54e10370edb71
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-07-01 10:37:23 -0400

    Update: ExternalLink  is now a styled component

--- src/components/Misc/ExternalLink/index.js
+++ src/components/Misc/ExternalLink/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import classnames from 'classnames';
-import styles from './ExternalLink.module.scss';
+import React, { useContext } from "react";
+import classnames from "classnames";
+import styles from "./ExternalLink.module.scss";
 
-const ExternalLink = ({text, icon, href}) => {
-    return(
-        <a href={ href }
-            className={ classnames("helpingMarginLR noWrap text-highlight", styles.textHighlight, styles.hvrSweepToRight) }
-            target="_blank"
-            rel="noopener noreferrer">
-            <strong>{ text } <i className={ icon }></i></strong>
-        </a>
-    )
-}
+import { ThemeContext } from "../../../contexts/ThemeContext";
+
+const ExternalLink = ({ text, icon, href }) => {
+  const { IsLightTheme, Theme } = useContext(ThemeContext);
+  const LocalTheme = IsLightTheme ? Theme.light : Theme.dark;
+
+  return (
+    <a
+      href={href}
+      className={classnames(
+        "helpingMarginLR noWrap text-highlight",
+        styles.textHighlight,
+        styles.hvrSweepToRight
+      )}
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{ backgroundColor: LocalTheme.textBackground }}
+    >
+      <strong style={{ color: LocalTheme.syntax }}>
+        {text} <i className={icon}></i>
+      </strong>
+    </a>
+  );
+};
 
 export default ExternalLink;
