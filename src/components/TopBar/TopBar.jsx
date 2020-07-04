a2d17340153a774f21d282afe6cfd4c7e3477631
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-07-04 19:23:54 -0400

    Refactor: TopBar component is now exported as default from its own index.jsx file

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import Slider from './Slider';
-import ColorBar from './ColorBar';
-import WicksLogo from './WicksLogo';
+import React from "react";
+import { Slider } from "./Slider";
+import { ColorBar } from "./ColorBar";
+import { WicksLogo } from "./WicksLogo";
 
 const TopBar = (props) => {
-
-    return(
-        <div>
-            <ColorBar />
-            <WicksLogo />
-            <Slider currentHourOfDay={ props.currentHourOfDay } />
-        </div>
-    )
-}
+  return (
+    <div>
+      <ColorBar />
+      <WicksLogo />
+      <Slider currentHourOfDay={props.currentHourOfDay} />
+    </div>
+  );
+};
 
 export default TopBar;
---
+++ src/components/TopBar/WicksLogo/index.jsx
@@ -0,0 +1,1 @@
+export { default as WicksLogo } from "./WicksLogo";
