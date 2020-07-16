dc73547c91950b4d45be8a22bba475ef4ec69cec
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-07-16 10:08:03 -0400

    Create: Report-A-Bug route

---
+++ src/Routes/ReportABug/ReportABug.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { SectionHeader } from "../../components/Misc/SectionHeader";
+
+const ReportABug = () => {
+  return <p>Reporting a bug!</p>;
+};
+
+export default ReportABug;
