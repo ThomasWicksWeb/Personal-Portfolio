33835c47238a2b9ceecec9fe5c7c8be86bf1b090
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-07-16 12:40:51 -0400

    Update: Added routes to a Report-A-Bug page and a 404 page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { TopBar } from "./components/TopBar";
 import { MainPage } from "./Routes/MainPage";
 import { ContactPage } from "./Routes/ContactPage";
+import { RouteNotFound } from "./Routes/RouteNotFound";
 import { Footer } from "./components/Footer";
 
 import "./style.scss";
+import { ReportABug } from "Routes/ReportABug";
 
 function App() {
   // Used for the dark mode slider in <TopBar /> and to set the greeting in <MainSplash />
@@ -24,6 +26,8 @@ function App() {
             exact
           />
           <Route path="/contact" component={ContactPage} exact />
+          <Route path="/report" component={ReportABug} exact />
+          <Route component={RouteNotFound} />
         </Switch>
       </BrowserRouter>
       <Footer />
