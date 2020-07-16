51580f0f1314f5f1fa0ff8f6bf5f57926defe9da
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-07-15 21:26:08 -0400

    Create: ContactPage route

---
+++ src/Routes/ContactPage/ContactPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ContactMe } from "../../components/ContactMe";
+
+const ContactPage = () => {
+  return (
+    <main>
+      <ContactMe />
+    </main>
+  );
+};
+
+export default ContactPage;
