25476441d71b2801fa3f32116ee6c746db8c9570
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-07-15 21:25:15 -0400

    Refactor: Ran Prettier

--- src/components/Misc/Emoji/Emoji.jsx
+++ src/components/Misc/Emoji/Emoji.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import classnames from 'classnames';
-import styles from './Emoji.module.scss';
+import React from "react";
+import classnames from "classnames";
+import styles from "./Emoji.module.scss";
 
-const Emoji = ({emoji, label, classes}) => {
-    return(
-        <span role="img" className={classnames(styles.emoji, classes)} aria-label={ label }>{ emoji }</span>
-    );
-}
+const Emoji = ({ emoji, label, classes }) => {
+  return (
+    <span
+      role="img"
+      className={classnames(styles.emoji, classes)}
+      aria-label={label}
+    >
+      {emoji}
+    </span>
+  );
+};
 
 export default Emoji;
