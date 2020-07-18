cfbe0de05f90330cb9ba1f3c54532c082074a2c7
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-07-17 20:18:04 -0400

    Update: Incorporated styled-components for a css hover state

--- src/components/TopBar/WicksLogo/WicksLogo.jsx
+++ src/components/TopBar/WicksLogo/WicksLogo.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import styles from "./WicksLogo.module.scss";
 import classnames from "classnames";
+import styled from "styled-components";
 
 import { ThemeContext } from "../../../contexts/ThemeContext";
 
@@ -9,16 +10,38 @@ const WicksLogo = () => {
   const { IsLightTheme, Theme } = useContext(ThemeContext);
   const LocalTheme = IsLightTheme ? Theme.light : Theme.dark;
 
+  let LogoTextWrapper = null;
+
+  if (IsLightTheme) {
+    LogoTextWrapper = styled.div`
+      transition: all 0.2s;
+      &:hover {
+        background-color: #333 !important;
+        color: #fefefe !important;
+      }
+    `;
+  } else {
+    LogoTextWrapper = styled.div`
+      transition: all 0.2s;
+      &:hover {
+        background-color: ${Theme.light.backgroundColorLight} !important;
+        color: #333 !important;
+      }
+    `;
+  }
+
   return (
     <Link to="/">
-      <div
+      <LogoTextWrapper
         id="LogoID"
         className={classnames("logoLight", styles.logoLight, styles.logo)}
         style={{ border: LocalTheme.wicksLogoBorder, color: LocalTheme.syntax }}
       >
-        <span>WI</span>
-        <span className={styles.negMargin}>CKS.</span>
-      </div>
+        <span className="has-font-weight-bold">WI</span>
+        <span className={classnames("has-font-weight-bold", styles.negMargin)}>
+          CKS.
+        </span>
+      </LogoTextWrapper>
     </Link>
   );
 };
