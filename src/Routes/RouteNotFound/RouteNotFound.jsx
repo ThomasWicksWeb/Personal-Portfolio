fedbf6145ce1967f1f716fa9a3fb3f772db6d812
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-07-17 20:18:19 -0400

    Update: Added a picture of Frank on the 404 page

--- src/Routes/RouteNotFound/RouteNotFound.jsx
+++ src/Routes/RouteNotFound/RouteNotFound.jsx
@@ -5,6 +5,7 @@ import classnames from "classnames";
 
 import { ThemeContext } from "../../contexts/ThemeContext";
 import styles from "./RouteNotFound.module.scss";
+import Frank404 from "./images/frank-404-min.jpg";
 
 const RouteNotFound = () => {
   const { IsLightTheme, Theme } = useContext(ThemeContext);
@@ -23,11 +24,19 @@ const RouteNotFound = () => {
           <h1 className="is-size-1 has-text-centered has-text-weight-bold">
             404!
           </h1>
+          <img
+            src={Frank404}
+            alt="Here's my dog though!"
+            className={styles.FrankImage}
+          />
           <h2 className="is-size-4 has-text-centered">
             Whoops! Looks like you took the wrong turn
           </h2>
           <div className="has-text-centered">
-            <Link to="/" className="is-size-6 button is-link has-text-weight-bold">
+            <Link
+              to="/"
+              className={classnames("is-size-6 button is-link has-text-weight-bold", styles.HeadBack)}
+            >
               Head back
             </Link>
           </div>
