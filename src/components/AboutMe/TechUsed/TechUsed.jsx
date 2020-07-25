7e24b92182c7a9bb678fab7e4db6a84112d53da4
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-07-24 21:32:32 -0400

    Created: TechUsed component for 'About Me' section

---
+++ src/components/AboutMe/TechUsed/TechUsed.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import classnames from "classnames";
+
+import styles from "./TechUsed.module.scss";
+
+const TechUsed = () => {
+  const TechItems = [
+    "Javascript (ES6+)",
+    "React",
+    "HTML & (S)CSS",
+    "Google Firebase",
+    "Tailwind CSS",
+    "Bulma",
+    // "Vue",
+  ];
+
+  return (
+    <div>
+      <h3
+        className={classnames(
+          "has-text-weight-bold is-size-3",
+          styles.TechUsedHeader
+        )}
+      >
+        Recent Tech Used
+      </h3>
+      <ul className={styles.ListContainer}>
+        {TechItems.map((item) => {
+          return <li className={classnames("is-size-6", styles.ListItem)}>{item}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default TechUsed;
---
+++ src/components/AboutMe/TechUsed/index.jsx
@@ -0,0 +1,1 @@
+export { default as TechUsed } from "./TechUsed";
