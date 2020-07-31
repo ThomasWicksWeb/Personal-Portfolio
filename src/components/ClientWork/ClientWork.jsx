3932a255015c046e7f6b2779480143a7844d4e8e
ThomasWicksWeb <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-07-31 13:18:58 -0400

    Update CSS grid colum values

--- src/components/ClientWork/ClientWork.jsx
+++ src/components/ClientWork/ClientWork.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import ClientsProjects from "./data/data";
+import { projectsLarge, projectsSmall } from "./data/data";
 import { ExternalLink } from "../Misc/ExternalLink";
 import { SectionHeader } from "../Misc/SectionHeader";
 
@@ -13,7 +13,7 @@ const ClientWork = () => {
   const LocalTheme = IsLightTheme ? Theme.light : Theme.dark;
 
   // Mapping each project in ClientWork
-  const LargeClientsProjectList = ClientsProjects.map((item) => {
+  const LargeClientsProjectList = projectsLarge.map((item) => {
     return (
       <article
         key={item.title}
@@ -63,17 +63,73 @@ const ClientWork = () => {
     );
   });
 
+  // Mapping projects that appear as small (German Word A day, Tesla Tracker)
+  const PPListSmall = projectsSmall.map((item) => {
+    return (
+      <article
+        key={item.title}
+        className="roundedCorners"
+        style={{ backgroundColor: LocalTheme.backgroundColorLight }}
+      >
+        <a
+          href={item.live}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link"
+        >
+          <img
+            src={item.img}
+            alt={item.imgAlt}
+            title="Drip on the Wall WIP Thumbnail"
+          />
+        </a>
+        <div
+          className="textContainer dottedWhenDark"
+          style={{ color: LocalTheme.syntax }}
+        >
+          <h2>
+            <strong style={{ color: LocalTheme.syntax }}>{item.title}</strong>
+          </h2>
+          <p className="is-size-5">{item.desc}</p>
+          <p className="is-size-6">
+            Technologies Used:{" "}
+            <strong style={{ color: LocalTheme.syntax }}>
+              {item.techUsed}
+            </strong>
+          </p>
+        </div>
+        <p className={classnames("is-size-6 ProjectLinkContainer")}>
+          <ExternalLink
+            href={item.live}
+            text={"View Project"}
+            icon={"fas fa-external-link-alt is-size-5"}
+          />
+          <ExternalLink
+            text={"View on GitHub"}
+            icon={"fab fa-github is-size-5"}
+            href={item.github}
+          />
+        </p>
+      </article>
+    );
+  });
+
   return (
     <section
       className="section"
       style={{ backgroundColor: LocalTheme.backgroundColorDark }}
     >
       <SectionHeader emoji="📁" label="Folder emoji" HeaderText="Client Work" />
-
       <div className="largeGridParent">
-        <div className="projectsLargeGrid invertDark">
-          {/* Output the mapped array of Large Projects */}
+        <div className="projectsLargeGrid">
+          {/* Large projects */}
           {LargeClientsProjectList}
+        </div>
+      </div>
+      <div className="smallGridParent">
+        <div className="projectsSmallGrid">
+          {/* Smaller projects */}
+          {PPListSmall}
         </div>
       </div>
     </section>
--- src/components/ClientWork/data/data.js
+++ src/components/ClientWork/data/data.js
@@ -1,5 +1,17 @@
 const projectsLarge = [
     {
+        title: "Fuller Fabrications (Work in Progress)",
+        desc: "Created an online presence for California-based company Fuller Fabrications",
+        img: "./images/FullerFab-min.jpg",
+        imgAlt: "Fuller Fabrications Thumbnail",
+        live: "https://boring-shaw-8df3a6.netlify.app/",
+        github: "https://github.com/ThomasWicksWeb/fullerfab_com",
+        techUsed: "React, TypeScript, Tailwind CSS"
+    },
+]
+
+const projectsSmall = [
+    {
         title: "Drip on the Wall (Work in Progress)",
         desc: "Drip on the Wall is a residential and commercial painting company based in California",
         img: "./images/DotW-min.jpg",
@@ -7,7 +19,7 @@ const projectsLarge = [
         live: "https://heuristic-johnson-ea569b.netlify.com/",
         github: "https://github.com/ThomasWicksWeb/DripOnTheWall",
         techUsed: "Bulma"
-    }
+    },
 ]
 
-export default projectsLarge;
+export { projectsLarge, projectsSmall };
