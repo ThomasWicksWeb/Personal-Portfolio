db366d24c290868b8ad559a8a1767a7e97590c0d
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-08-05 22:37:11 -0400

    Update wrap entire application in Hours Context Provider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import ThemeContextProvider from "./contexts/ThemeContext";
+import HoursContextProvider from "./contexts/HoursContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeContextProvider>
-      <App />
+      <HoursContextProvider>
+        <App />
+      </HoursContextProvider>
     </ThemeContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
