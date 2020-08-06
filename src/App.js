90d47716c1487ed2efb344a7a1ea83203eb8ecb7
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-08-05 22:36:13 -0400

    Update moved currentHourOfDay to its own context

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import { TopBar } from "./components/TopBar";
+// Routes
 import { MainPage } from "./Routes/MainPage";
+import { ReportABug } from "Routes/ReportABug";
 import { ContactPage } from "./Routes/ContactPage";
 import { RouteNotFound } from "./Routes/RouteNotFound";
+
+// Components
+import { TopBar } from "./components/TopBar";
 import { Footer } from "./components/Footer";
 
+// Styles
 import "./style.scss";
-import { ReportABug } from "Routes/ReportABug";
 
 function App() {
-  // Used for the dark mode slider in <TopBar /> and to set the greeting in <MainSplash />
-  const currentHourOfDay = new Date().getHours();
-
   return (
     <main>
       <BrowserRouter>
-        <TopBar currentHourOfDay={currentHourOfDay} />
+        <TopBar />
         <Switch>
-          <Route
-            path="/"
-            component={MainPage}
-            currentHourOfDay={currentHourOfDay}
-            exact
-          />
+          <Route path="/" component={MainPage} exact />
           <Route path="/contact" component={ContactPage} exact />
           <Route path="/report" component={ReportABug} exact />
           <Route component={RouteNotFound} />
