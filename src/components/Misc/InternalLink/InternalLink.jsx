f449d4c504594c8670ff231a562b6ca97151b9ba
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-08-27 13:35:54 -0400

    Update converted to a styled-component

--- src/components/Misc/InternalLink/InternalLink.jsx
+++ src/components/Misc/InternalLink/InternalLink.jsx
@@ -1,42 +1,39 @@
-import React, { useContext } from "react";
-import classnames from "classnames";
+import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-import styles from "./InternalLink.module.scss";
 
-import { ThemeContext } from "../../../contexts/ThemeContext";
-
-const InternalLink = ({ text, href, emoji }) => {
-  const { IsLightTheme, Theme } = useContext(ThemeContext);
-  const LocalTheme = IsLightTheme ? Theme.light : Theme.dark;
-
-  let InternalLinkWrapper = null;
-
-  if (IsLightTheme) {
-    InternalLinkWrapper = styled.span`
-      &::after {
-        background-color: rgba(255, 147, 0, 0.5);
-      }
-    `;
-  } else {
-    InternalLinkWrapper = styled.span`
-      &::after {
-        background-color: #e54242;
-      }
-    `;
-  }
+export default function InternalLink({ text, to }){
+  const Wrapper = styled.span`
+    position: relative;
+    color: #333;
+    padding: 4px 8px;
+    margin: 0 0.5rem;
+    &::after {
+      content: "" !important;
+      position: absolute !important;
+      z-index: -1 !important;
+      top: 70% !important;
+      left: -0.1px !important;
+      right: -0.1px !important;
+      bottom: 0 !important;
+      transition: top 0.1s ease-in-out;
+      background-color: rgba(255, 147, 0, 0.5) !important;
+    }
+    &:hover::after {
+      top: 0 !important;
+    }
+    // zFix
+    z-index: 0 !important;
+  `;
 
   return (
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href={href}
-      className={classnames("noWrap zFix", styles.zFix)}
+    <Link
+      to={to}
+      onClick={() => window.scroll({ top: 0, left: 0, behavior: "smooth" })}
     >
-      <InternalLinkWrapper className={styles.borderMarker}>
-        <strong style={{ color: LocalTheme.syntax }}>{text}</strong> {emoji}
-      </InternalLinkWrapper>
-    </a>
+      <Wrapper>
+        <strong>{text}</strong>
+      </Wrapper>
+    </Link>
   );
 };
-
-export default InternalLink;
